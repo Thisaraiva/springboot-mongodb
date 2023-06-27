2563785f96898d8a0932fb82d591418d84643af9
Thisaraiva <thiagofreitassaraiva@yahoo.com.br>
2023-06-27 17:04:40 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/thiagosaraiva/springbootmongodb/SpringbootmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.thiagosaraiva.springbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmongodbApplication.class, args);
+	}
+
+}
