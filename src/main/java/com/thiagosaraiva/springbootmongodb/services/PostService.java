74d725cca6abaee06c6b7938e66a717c295dd459
Thisaraiva <thiagofreitassaraiva@yahoo.com.br>
2023-06-28 16:43:12 -0300

    Obtendo um post por id

---
+++ src/main/java/com/thiagosaraiva/springbootmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.thiagosaraiva.springbootmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagosaraiva.springbootmongodb.domain.Post;
+import com.thiagosaraiva.springbootmongodb.repository.PostRepository;
+import com.thiagosaraiva.springbootmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
