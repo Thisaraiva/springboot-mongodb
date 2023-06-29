13abb4db458a86120b75112f06031852289802cc
Thisaraiva <thiagofreitassaraiva@yahoo.com.br>
2023-06-28 21:54:09 -0300

    Consulta simples com query methods

--- src/main/java/com/thiagosaraiva/springbootmongodb/repository/PostRepository.java
+++ src/main/java/com/thiagosaraiva/springbootmongodb/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.thiagosaraiva.springbootmongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.thiagosaraiva.springbootmongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);// com IgnoreCase ele desconsidera a pesquisa com diferença entre letras maiúsculas e minúsculas
 
 }
