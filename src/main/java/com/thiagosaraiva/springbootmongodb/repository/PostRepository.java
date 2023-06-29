d195b0b6e0722f8dcccb310fb5244484fc963d8e
Thisaraiva <thiagofreitassaraiva@yahoo.com.br>
2023-06-28 22:01:40 -0300

    Consulta simples com @Query

--- src/main/java/com/thiagosaraiva/springbootmongodb/repository/PostRepository.java
+++ src/main/java/com/thiagosaraiva/springbootmongodb/repository/PostRepository.java
@@ -3,12 +3,17 @@ package com.thiagosaraiva.springbootmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.thiagosaraiva.springbootmongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	//o "?0" significa que o parametro será o primeiro de entrada que no caso será o parâmetro "text"
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);// com IgnoreCase ele desconsidera a pesquisa com diferença entre letras maiúsculas e minúsculas
 
